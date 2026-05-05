1eb5340efe36b87a1c0feeba4064c9545842102a
sleekEagle <lahirunuwanisme@gmail.com>
2026-05-05 16:12:39 +0530

    write eval results to file

--- eval_importance.py
+++ eval_importance.py
@@ -6,6 +6,7 @@ import torch
 import os
 from pathlib import Path
 import CONST
+import random
 
 #get all pred original logits from the group log file
 def get_orig_logits(PATH):
@@ -21,19 +22,23 @@ def get_orig_logits(PATH):
     return d
 
 class EvalLogits:
-    def __init__(self, full_video, model, groups, ol, cls_idx):
+    def __init__(self, full_video, model, groups, ol, cls_idx, show_mask=False):
         self.full_video = full_video
         self.model = model
         self.groups = groups
         self.ol = ol
         self.cls_idx = cls_idx
+        self.show_mask = show_mask
 
-    def eval(self, idx_order):
+    def eval_remove(self, idx_order):
         logits = [self.ol]
         mask = [1]*len(self.groups)
-        for idx in idx_order:
-            mask[idx]=0
+        if self.show_mask:
             print(mask)
+        for idx in idx_order:
+            mask[idx] = 0
+            if self.show_mask :
+                print(mask)
             if sum(mask) == 0:
                 logits.append(CONST.UCF_AVG_PRED[self.cls_idx].item())
                 continue
@@ -45,10 +50,30 @@ class EvalLogits:
                 l = p[0,self.cls_idx].item()
                 logits.append(l)
         return logits
+    
+    
+    def eval_add(self, idx_order):
+        logits = []
+        mask = [0]*len(self.groups)
+        if self.show_mask:
+            print(mask)
+        logits.append(CONST.UCF_AVG_PRED[self.cls_idx].item())
+        for idx in idx_order:
+            mask[idx] = 1
+            if self.show_mask:
+                print(mask)
+            g = func.past_fill_all(mask, self.groups)
+            vid_g = func.create_grouped_video(self.full_video.permute(1,0,2,3), g)
+            with torch.no_grad():
+                p = self.model(vid_g[None,:])
+                l = p[0,self.cls_idx].item()
+                logits.append(l)
+        return logits
 
 def eval_UCF101():
     IMP_PATH = r'C:\Users\lahir\Downloads\UCF101\analysis\exactSHAP_0.001.jsonl'
     GRP_PATH = r'C:\Users\lahir\Downloads\UCF101\analysis\groups_0.001.jsonl'
+    OUT_PATH = r'C:\Users\lahir\Downloads\UCF101\analysis\eval_0.001.jsonl'
 
     orig_logits = get_orig_logits(GRP_PATH)
 
@@ -63,8 +88,11 @@ def eval_UCF101():
     with open(IMP_PATH, 'r', encoding='utf-8') as f:
         line_count = sum(1 for _ in enumerate(f))
 
+    n=0
     with open(IMP_PATH, 'r', encoding='utf-8') as f:
         for line in f:
+            print(f'{n/line_count*100:.1f}% is done.')
+            n+=1
             line = line.strip()
             if not line:
                 continue
@@ -85,7 +113,7 @@ def eval_UCF101():
             assert len(record['groups']) == len(sv) , 'Number of shapley values do not match!'
 
             sv_c = [s[cls_idx] for s in sv]
-            sort_idx = [int(i) for i in np.argsort(sv_c)]
+            asc_idx = [int(i) for i in np.argsort(sv_c)]
 
             # let groups have integer keys
             groups = {}
@@ -94,20 +122,22 @@ def eval_UCF101():
 
             el = EvalLogits(video, model, groups, ol, cls_idx)
 
-            
-            el.eval(sort_idx)
-            pass
-            
+            results = {
+                'filename': record['filename'],
+                'rmv_asc': el.eval_remove(asc_idx),
+                'rmv_dec': el.eval_remove(asc_idx[::-1]),
+                'rmv_rand': el.eval_remove(random.sample(asc_idx, len(asc_idx))),
+                'rmv_lr': el.eval_remove(sorted(asc_idx)),
+                'rmv_rl': el.eval_remove(sorted(asc_idx)[::-1]),
+                'add_asc': el.eval_add(asc_idx),
+                'add_dec': el.eval_add(asc_idx[::-1]),
+                'add_rand': el.eval_add(random.sample(asc_idx, len(asc_idx))),
+                'add_lr': el.eval_add(sorted(asc_idx)),
+                'add_rl': el.eval_add(sorted(asc_idx)[::-1])
+            }
 
-
-
-
-
-
-
-            pass
-
-    pass
+            with open(OUT_PATH, 'a') as f:
+                f.write(json.dumps(results) + '\n')
 
 if __name__ == "__main__":
     eval_UCF101()
